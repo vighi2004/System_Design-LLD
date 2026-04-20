aefd49988cfc9a4124c6aafe09b4c96864402fb3
vighi2004 <vighneshtakke4@gmail.com>
2026-04-20 13:32:03 +0530

    add

---
+++ L1.java
@@ -0,0 +1,20 @@
+//breaking rule of history contraints;
+class immu{
+   protected int x,y;
+   public immu(int x,int y){
+       this.x=x;
+       this.y=y;
+   }
+}
+//breaking rule function.    
+// 
+// public class mu extends immu {
+//       int x ,y;
+//       public mu(int x){//breaking history constraints.
+//           this.x=x;
+//       }
+public class L1{
+    public static void main(String[] args) {
+        
+    }
+}
