15678f79da4ceb4c8306bb91f348403f22abc8e1
vighi2004 <vighneshtakke4@gmail.com>
2026-04-26 13:00:13 +0530

    singelton design pattern

--- singeltonDesign.java
+++ singeltonDesign.java
@@ -1,13 +1,22 @@
 //Implementing singelton design pattern here.
 class Singelton{
-    public Singelton(){
+    private static Singelton Instance =null;
+    private Singelton(){
         System.out.println("Creating a new instance of Singelton class.");
+    }
+    //getters using
+    //making thrad safe now using synchronized keyword.
+    public static  synchronized Singelton getInstance(){
+        if(Instance==null){
+            Instance=new Singelton();
+        }
+        return Instance;
     }
 }
 public class singeltonDesign{
     public static void main(String[] args) {
-        Singelton S=new Singelton();
-        Singelton S1=new Singelton();
+        Singelton S=Singelton.getInstance();
+        Singelton S1=Singelton.getInstance();
         if(S==S1){
             System.out.println("Both S and S1 are the same instance.");
         }
